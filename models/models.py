83662fad954e5c7de51f35e51c6d240638fec1cc
Nay3li <nayeli.valencia@xmarts.com.mx>
2018-01-04 15:39:53 -0600

    ajustes de facturas

--- models/models.py
+++ models/models.py
@@ -2,7 +2,6 @@
 
 from openerp import models, fields, api, _, tools
 from openerp.exceptions import UserError, RedirectWarning, ValidationError
-import xlrd
 import shutil
 import logging
 _logger = logging.getLogger(__name__)
