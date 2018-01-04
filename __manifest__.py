a0fde3a617a5b24c8fd217de80c6abbe165b8779
Nay3li <nayeli.valencia@xmarts.com.mx>
2018-01-04 13:58:44 -0600

    Factura personalizada de xmarts

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Reporte Xmarts',
+    'summary': ' Adaptaciones a la plantilla box de odoo 11',
+
+    'description': """
+    - Adaptaciones a la plantilla box de odoo 11
+    """,
+    'author': "Xmarts, Nayeli Valencia Díaz",
+    'website': "http://www.xmarts.com",
+    'depends': ['base'],
+    'data': [
+        'reports/report_invoice.xml'
+    ]
+}
